10c74cc4d95c3f2d1b9cbf7619bea6890c38c4dc
Ravi Lakhotia <ravilakhotia2006@gmail.com>
2020-09-28 13:13:22 +0530

    Added basic html to home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls.resolvers import get_resolver
+
 from lists.views import home_page
 
 
@@ -13,3 +15,12 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self) -> None:
         found = get_resolver().resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self) -> None:
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
