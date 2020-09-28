dd5bf42aef43fbe059569f0c7ea7967479b16ac5
Ravi Lakhotia <ravilakhotia2006@gmail.com>
2020-09-28 13:03:32 +0530

    Added unit test for home page resolve

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls.resolvers import get_resolver
+from lists.views import home_page
 
-# Create your tests here.
+
+class SmokeTest(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_root_url_resolves_to_home_page_view(self) -> None:
+        found = get_resolver().resolve('/')
+        self.assertEqual(found.func, home_page)
