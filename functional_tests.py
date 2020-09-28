5b1a99f8733be01e0a28810f4286baeee23c5a2e
Ravi Lakhotia <ravilakhotia2006@gmail.com>
2020-09-28 12:25:03 +0530

    Added lists apps and updated gitignore to ignore venv, idea files

--- functional_tests.py
+++ functional_tests.py
@@ -10,7 +10,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def test_can_start_a_list_and_retreive_it_later(self) -> None:
+    def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
